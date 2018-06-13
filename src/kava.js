3fc732d2a66ceef905c8924fcaaf8a68128be2d3
Dan Ziv <dan.ziv@kaltura.com>
2018-06-13 11:54:35 +0300

    FEC-8306

--- src/kava.js
+++ src/kava.js
@@ -162,12 +162,10 @@ export default class Kava extends BasePlugin {
       this._onSourceSelected()
     );
     this.eventManager.listen(this.player, this.player.Event.ERROR, event => this._onError(event));
+    this.eventManager.listen(this.player, this.player.Event.FIRST_PLAY, () => this._onFirstPlay());
     this.player.ready().then(() => {
       this._setInitialTracks();
       this.eventManager.listen(this.player, this.player.Event.PLAYING, () => this._onPlaying());
-      this.eventManager.listen(this.player, this.player.Event.FIRST_PLAY, () =>
-        this._onFirstPlay()
-      );
       this.eventManager.listen(this.player, this.player.Event.SEEKING, () => this._onSeeking());
       this.eventManager.listen(this.player, this.player.Event.PAUSE, () => this._onPause());
       this.eventManager.listen(this.player, this.player.Event.ENDED, () => this._onEnded());
