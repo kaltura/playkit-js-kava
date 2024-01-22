c7e3782a4548d7fadb3036af6d6402ca41f43bf1
JonathanTGold <jonathan.gold@86@gmail.com>
2024-01-22 16:27:08 +0200

    Move plugins to type script - kaltura/playkit-js-kava

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,72 @@
-'use strict';
-
 const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
 
-const plugins = [
-  new webpack.DefinePlugin({
-    __VERSION__: JSON.stringify(packageData.version),
-    __NAME__: JSON.stringify(packageData.name)
-  })
-];
-
-module.exports = {
-  context: __dirname + '/src',
-  entry: {
-    'playkit-kava': 'index.js'
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: ['KalturaPlayer', 'plugins', 'kava'],
-    devtoolModuleFilenameTemplate: './kava/[resource-path]'
-  },
-  devtool: 'source-map',
-  plugins: plugins,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ],
-        exclude: [/node_modules/]
-      },
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        enforce: 'pre',
-        use: [
-          {
-            loader: 'eslint-loader',
+module.exports = (env, { mode }) => {
+  return {
+    target: 'web',
+    entry: './src/index.ts',
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              rules: {
-                semi: 0
-              }
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    bugfixes: true
+                  }
+                ],
+                '@babel/preset-typescript'
+              ],
+              plugins: [['@babel/plugin-transform-runtime']]
             }
           }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: __dirname + '/src'
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    alias: {
-      '@playkit-js/related': path.resolve(`node_modules/@playkit-js/related/types`),
-      '@playkit-js/share': path.resolve(`node_modules/@playkit-js/share/types`),
-      '@playkit-js/playkit-js-downloads': path.resolve(`node_modules/@playkit-js/playkit-js-downloads/types`),
-      '@playkit-js/info': path.resolve(`node_modules/@playkit-js/info/types`),
-      '@playkit-js/moderation': path.resolve(`node_modules/@playkit-js/moderation/types`)
+        },
+      ]
     },
-    extensions: ['.js']
-  },
-  externals: {
-    '@playkit-js/kaltura-player-js': ['KalturaPlayer'],
-    '@playkit-js/playkit-js-providers': ['KalturaPlayer', 'providers']
-  }
+    resolve: {
+      extensions: ['.ts', '.js'],
+      alias: {
+        '@playkit-js/related': path.resolve(`node_modules/@playkit-js/related/types`),
+        '@playkit-js/share': path.resolve(`node_modules/@playkit-js/share/types`),
+        '@playkit-js/playkit-js-downloads': path.resolve(`node_modules/@playkit-js/playkit-js-downloads/types`),
+        '@playkit-js/info': path.resolve(`node_modules/@playkit-js/info/types`),
+        '@playkit-js/moderation': path.resolve(`node_modules/@playkit-js/moderation/types`)
+      },
+    },
+    output: {
+      filename: 'playkit-kava.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        umdNamedDefine: true,
+        name: ['KalturaPlayer', 'plugins', 'kava'],
+        type: 'umd'
+      },
+      clean: true
+    },
+    externals: {
+      '@playkit-js/kaltura-player-js': ['KalturaPlayer'],
+      '@playkit-js/playkit-js-providers': ['KalturaPlayer', 'providers']
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist')
+      },
+      client: {
+        progress: true
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VERSION__: JSON.stringify(packageData.version),
+        __NAME__: JSON.stringify(packageData.name)
+      })
+    ]
+  };
 };
