94e0fa204124ec865ed1b2f3d8d81716126ac8c7
JonathanTGold <jonathan.gold@86@gmail.com>
2024-03-05 14:21:43 +0100

    add bindings

--- src/application-events-model.ts
+++ src/application-events-model.ts
@@ -1,17 +1,17 @@
-import { DownloadEvents, DualscreenEvents, ShareEvents } from "./new-applications-events";
-import { KavaModel } from "./kava-model";
-import { KavaEvent } from "./types";
-import { ButtonType } from "./enums/button-type";
+import { DownloadEvents, DualscreenEvents, ShareEvents } from './new-applications-events';
+import { KavaModel } from './kava-model';
+import { KavaEvent } from './types';
+import { ButtonType } from './enums/button-type';
 
 const EVENT_TYPE_BUTTON_CLICK = 10002;
 
-function getApplicationEventsModel(eventObj: KavaEvent, model: KavaModel, innerEventPayload: any): any {
+export function getApplicationEventsModel(eventObj: KavaEvent, model: KavaModel, innerEventPayload: any): any {
   const commonModel = {
     eventType: EVENT_TYPE_BUTTON_CLICK,
     partnerId: model.getPartnerId(),
     entryId: model.getEntryId(),
-    sessionId: model.getSessionId(),
-  }
+    sessionId: model.getSessionId()
+  };
   if (model.getUserId()) {
     commonModel['userId'] = model.getUserId();
   }
@@ -19,105 +19,111 @@ function getApplicationEventsModel(eventObj: KavaEvent, model: KavaModel, innerE
   return Object.assign(eventModel, commonModel);
 }
 
-
-export const ApplicationEventsModel = {
+export const ApplicationEventsModel:  {[playerEventName: string]: KavaEvent} = {
   [DualscreenEvents.CHANGE_LAYOUT]: {
+    type: 'CHANGE_LAYOUT',
     getEventModel: (payload: any): any => ({
       buttonName: 'Dual__screen_change_layout',
       buttonType: ButtonType.Unknown,
-      buttonValue: payload.layout,
+      buttonValue: payload.layout
     })
   },
   [ShareEvents.SHARE_CLICKED]: {
+    type: 'SHARE_CLICKED',
     getEventModel: (payload: any): any => ({
       buttonName: 'Share_embed_open',
       buttonType: ButtonType.Open,
-      buttonValue: payload.layout,
+      buttonValue: ''
     })
   },
   [ShareEvents.SHARE_CLOSE]: {
+    type: 'SHARE_CLOSE',
     getEventModel: (payload: any): any => ({
       buttonName: 'Share_embed_close',
       buttonType: ButtonType.Close,
-      buttonValue: payload.layout,
+      buttonValue: ''
     })
   },
   [ShareEvents.SHARE_NETWORK]: {
+    type: 'SHARE_NETWORK',
     getEventModel: (payload: any): any => {
       const model = {
         buttonType: ButtonType.Share,
-        buttonValue: '',
-      }
+        buttonValue: ''
+      };
       let buttonName: string;
 
       switch (payload.socialNetworkName) {
-      case "twitter":
-        buttonName = 'Share_embed_X_click'
-        break;
-      case "facebook":
-        buttonName = 'Share_embed_facebook_click'
-        break;
-      case "email":
-        buttonName = 'Share_embed_email_click'
-        break;
-      case "linkedin":
-        buttonName = 'Share_embed_linkedin_click'
-        break;
-      default:
-        buttonName = 'unknown'
+        case 'twitter':
+          buttonName = 'Share_embed_X_click';
+          break;
+        case 'facebook':
+          buttonName = 'Share_embed_facebook_click';
+          break;
+        case 'email':
+          buttonName = 'Share_embed_email_click';
+          break;
+        case 'linkedin':
+          buttonName = 'Share_embed_linkedin_click';
+          break;
+        default:
+          buttonName = 'unknown';
       }
 
-      return {...model, buttonName}
+      return { ...model, buttonName };
     }
   },
   [ShareEvents.SHARE_COPY]: {
+    type: 'SHARE_COPY',
     getEventModel: (payload: any): any => ({
       buttonName: 'Share_embed_X_click',
       buttonType: ButtonType.Share,
-      buttonValue: payload.layout,
+      buttonValue: payload.videoClippingOption
     })
   },
   [DownloadEvents.DOWNLOAD_ITEM_CLICKED]: {
+    type: 'DOWNLOAD_ITEM_CLICKED',
     getEventModel: (payload: any): any => {
       const model = {
-        buttonType: ButtonType.Download,
-      }
-      const {assetType, fileType, description} = payload;
+        buttonType: ButtonType.Download
+      };
+      const { assetType, fileType, description } = payload;
 
       let buttonName: string;
       let buttonValue = description;
 
       switch (assetType) {
-      case "Media":
-        buttonName = 'Download_video_download';
-        break;
-      case "Captions":
-        buttonName = 'Download_captions_download';
-        break;
-      case "Attachments":
-        buttonName = 'Download_attachment_download';
-        buttonValue = fileType;
-        break;
-      default:
-        buttonName = ''
-        buttonValue = '';
+        case 'Media':
+          buttonName = 'Download_video_download';
+          break;
+        case 'Captions':
+          buttonName = 'Download_captions_download';
+          break;
+        case 'Attachments':
+          buttonName = 'Download_attachment_download';
+          buttonValue = fileType;
+          break;
+        default:
+          buttonName = '';
+          buttonValue = '';
       }
-
-      return {...model, buttonName, buttonValue}
+      return { ...model, buttonName, buttonValue };
     }
   },
   [DownloadEvents.SHOW_OVERLAY]: {
+    type: 'SHOW_OVERLAY',
     getEventModel: (payload: any): any => ({
       buttonName: 'Download_open',
       buttonType: ButtonType.Open,
-      buttonValue: '',
+      buttonValue: ''
     })
   },
   [DownloadEvents.HIDE_OVERLAY]: {
+    type: 'HIDE_OVERLAY',
     getEventModel: (payload: any): any => ({
       buttonName: 'Download_close',
       buttonType: ButtonType.Close,
-      buttonValue: '',
+      buttonValue: ''
     })
-  },
+  }
 };
---
+++ src/enums/event-bucket-name.ts
@@ -0,0 +1,4 @@
+export enum EventBucketName {
+  ApplicationEvents = 'ApplicationEvents',
+  PlayerEvents = 'PlayerEvents',
+}
