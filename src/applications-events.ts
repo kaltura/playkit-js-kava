074a52b54748c4bfa874e613e05ab67e2682072d
roeedean <roee.dean@kaltura.com>
2025-12-21 15:30:45 +0200

    remarks

--- src/applications-events.ts
+++ src/applications-events.ts
@@ -167,7 +167,7 @@ export const KavaEvents = {
 export const GenieEvents = {
   GENIE_OPEN: 'genie_open_plugin',
   GENIE_CLOSE: 'genie_close_plugin',
-  NEW_THREAD: 'new_thread'
+  GENIE_NEW_THREAD: 'new_thread'
 };
 
 export const PluginsEvents = {
